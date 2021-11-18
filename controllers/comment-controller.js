bd22670ca9623c0fda6e4c2511d8846c3409a493
Kenzi Nielsen <kenzinielsen@gmail.com>
2021-11-18 16:47:46 -0600

    comments data added

---
+++ controllers/comment-controller.js
@@ -0,0 +1,49 @@
+const { Comment, Pizza } = require('../ models');
+const { db } = require('../ models/Comment');
+
+const commentController = {
+    //add commment to pizza
+    addComment({ params, body },res) {
+        console.log(body);
+        Comment.create(body)
+        .then(({ _id }) => {
+            return Pizza.findOneAndUpdate(
+            { _id: params.pizzaId },
+            { $push: { comments: _id } },
+            { new: true }
+            );
+        })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => res.json(err));
+    },
+    //remove
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+        .them(deletedComment => {
+            if (!deletedComment) {
+                return res.status(404).json({ message: 'no comment with this id'})
+            }
+            return Pizza.findOneAndUpdate(
+                { _id: params.pizzaId },
+                { $pull: { comments: params.commentId } },
+                { new: true }
+            )
+        })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id' })
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+            .catch(err => res.json(err));
+    }
+};
+
+module.exports = commentController;
