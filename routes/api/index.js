8aecc5d4d044fda5d9641448d03e614d0e43d301
Kenzi Nielsen <kenzinielsen@gmail.com>
2021-11-18 14:33:49 -0600

    server.js update

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
 
-//add prefix of /pizzas to routes created in pizza-routesjs
 router.use('/pizzas', pizzaRoutes);
 
-module.exports = router; 
+module.exports = router;
